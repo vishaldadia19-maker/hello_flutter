c8f3644f54cdcb9423a1b81cde8384b2e4e48da2
Vishal Dadia <vishaldadia@Vishals-MacBook-Air.local>
2026-02-11 01:19:40 +0530

    Fix Firebase iOS init to use plist instead of options

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if (kIsWeb) {
+    // Web needs explicit options
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } else {
+    // iOS & Android use native config files
+    await Firebase.initializeApp();
+  }
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'FIREBASE CORE OK',
-            style: TextStyle(fontSize: 24),
-          ),
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text(
+          'FIREBASE CORE OK',
+          style: TextStyle(fontSize: 24),
         ),
       ),
-    );
-  }
+    ),
+  ));
 }
